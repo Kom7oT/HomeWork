eb12666cd021a373bd9e64584cae9e4191c47c5f
Kom7oT <86073918+Kom7oT@users.noreply.github.com>
2021-07-03 11:24:42 +0300

    hw6

--- src/HomeWork6/Animals/Animal.java
+++ src/HomeWork6/Animals/Animal.java
@@ -1,4 +1,26 @@
 package HomeWork6.Animals;
 
-public class Animal {
+public abstract class Animal {
+   protected String name;
+   protected String color;
+   protected int age;
+   private static int count;
+
+
+    public Animal(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        count++;
+    }
+
+    public void run(int distanceRun) {
+    }
+
+   public void swim(int distanceSwim){
+
+    }
+    public static void getCount(){
+        System.out.printf("Animals created: %d\n", count);
+    }
 }
